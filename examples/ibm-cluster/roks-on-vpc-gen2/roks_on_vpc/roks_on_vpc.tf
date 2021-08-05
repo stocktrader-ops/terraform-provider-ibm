f4bc97a549bbc590d17965c7e900c49df61e4f8d
gasgithub <gasgithub@users.noreply.github.com>
2021-08-05 17:38:42 +0200

    Update roks_on_vpc.tf
    removing worker pool

--- examples/ibm-cluster/roks-on-vpc-gen2/roks_on_vpc/roks_on_vpc.tf
+++ examples/ibm-cluster/roks-on-vpc-gen2/roks_on_vpc/roks_on_vpc.tf
@@ -97,20 +97,4 @@ resource "ibm_container_vpc_cluster" "cluster" {
 
 }
 
-resource "ibm_container_vpc_worker_pool" "cluster_pool" {
-  cluster           = ibm_container_vpc_cluster.cluster.id
-  worker_pool_name  = "${var.worker_pool_name}${random_id.name1.hex}"
-  flavor            = var.flavor
-  vpc_id            = ibm_is_vpc.vpc1.id
-  worker_count      = var.worker_count
-  resource_group_id = data.ibm_resource_group.resource_group.id
-  entitlement       = var.entitlement
-  zones {
-    subnet_id = ibm_is_subnet.subnet1.id
-    name      = local.ZONE1
-  }
-  zones {
-    subnet_id = ibm_is_subnet.subnet2.id
-    name      = local.ZONE2
-  }
-}
+
